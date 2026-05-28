b04626d1a4c45f16c82832bd601d3a615feb37a9
Pokidj <Javipoki@gmail.com>
2026-05-28 13:55:39 +0200

    cambios a la hora de cargar desde google cambios en estado

---
+++ senorpp_config.js
@@ -0,0 +1,5 @@
+window.SENORPP_CONFIG = {
+  // Pega aqui tu URL /exec de Google Apps Script y sube este archivo junto al HTML.
+  // Ejemplo: "https://script.google.com/macros/s/XXXXX/exec"
+  googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbxDhEhwHdREWEMSG-t1SAbtMEKls3A7LhNcQlowNkWpyxdIKPzQD9aDhat1Q9pO07bS/exec"
+};
