b782374756bc012df92dcd27159a58b25a824b94
Pokidj <Javipoki@gmail.com>
2026-05-28 16:00:02 +0200

    Update senorp_config.js

--- senorp_config.js
+++ senorp_config.js
@@ -1,5 +1,5 @@
 window.SENORP_CONFIG = {
   // Pega aqui tu URL /exec de Google Apps Script y sube este archivo junto al HTML.
   // Ejemplo: "https://script.google.com/macros/s/XXXXX/exec"
-  googleAppsScriptUrl: ""
+  googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbxDhEhwHdREWEMSG-t1SAbtMEKls3A7LhNcQlowNkWpyxdIKPzQD9aDhat1Q9pO07bS/exec"
 };
